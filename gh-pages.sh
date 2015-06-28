6120ae74d8e69729cdb4b1d78fec21150f0e86be
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-06-28 16:51:04 -0500

    some additional cleanup of deploy script
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,15 +5,13 @@ git config --global user.name "George K. Thiruvathukal"
 git remote add deploy "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
 git fetch deploy
 git reset deploy/gh-pages
-rm -rf build
 git checkout master source Makefile build-handbook.sh
-git reset HEAD
+rm -rf build
 ./build-handbook.sh
-mv -fv build/html/* ./
 mkdir -p download
+mv -fv build/html/* ./
 mv -fv build/latex/*.pdf ./download/
 mv -fv build/epub/*.epub ./download/
-# rm -rf build
 touch .nojekyll
 git add -A
 git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push deploy HEAD:gh-pages
