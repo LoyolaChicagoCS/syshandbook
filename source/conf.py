99f6628083b2240edf30b96819de7d95071c4ccf
George K. Thiruvathukal <gkt@cs.luc.edu>
2019-03-05 13:55:11 -0600

    remove sphinx_bootstrap_theme from path configuration (conf.py)

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,6 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_bootstrap_theme
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -97,7 +96,7 @@ pygments_style = 'sphinx'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
