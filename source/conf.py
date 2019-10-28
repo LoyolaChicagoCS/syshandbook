bab358d7efd9781713a74d6ea660605f278bde08
George K. Thiruvathukal <gkt@cs.luc.edu>
2019-10-27 20:53:46 -0500

    add Google analytics info

--- source/conf.py
+++ source/conf.py
@@ -95,6 +95,23 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    #'canonical_url': '',
+    'analytics_id': 'UA-5482792-29'
+    #'logo_only': False,
+    #'display_version': True,
+    #'prev_next_buttons_location': 'bottom',
+    #'style_external_links': False,
+    #'vcs_pageview_mode': '',
+    #'style_nav_header_background': 'white',
+    # Toc options
+    #'collapse_navigation': True,
+    #'sticky_navigation': True,
+    #'navigation_depth': 4,
+    #'includehidden': True,
+    #'titles_only': False
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
